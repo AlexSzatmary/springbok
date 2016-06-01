615273aee1b6b15901095553cd24f6862bbd8dc8
Alex C. Szatmary <alex.szatmary@gmail.com>
2016-05-31 22:29:36 -0400

    Like with vary_gamma_L_2, this case uses baseline r_L that maximize relay

--- n_signal_relay/vary_gamma_L.py
+++ n_signal_relay/vary_gamma_L.py
@@ -12,8 +12,11 @@ import runner
 #L_gamma_L = [400., 40., 4., 1/1.5, 0.4, 0.04, 0.004]
 L_gamma_L = [5e2, 2e2, 1e2, 5e1, 2e1, 1e1, 5e0, 2e0, 1e0, 5e-1, 2e-1, 1e-1, 5e-2, 2e-2, 1e-2, 5e-3, 2e-3, 1e-3]
 #L_gamma_L = [5e-1, 2e-1, 1e-1]
-L_r_L = [1e0, 1e1, 1e2, 1e3]
+L_L_00 = [1e-1, 1e0, 1e1]
 L_phi_E = [0., 0.5, 1.]
+
+# rough values taken from n_exo-2016-05-27
+d_r_L = {0.: 50., 0.25: 75., 0.5: 100., 0.75: 100., 1.: 200.}
 
 #Required by Meerkat
 L_variables = [L_gamma_L, L_r_L, L_phi_E]
@@ -23,10 +26,12 @@ one_job = False
 if one_job:
     L_variables = [[L[0]] for L in L_variables]
 
-def job_name(gamma_L, r_L, phi_E):
-    return 'vary_gamma_L' + str(gamma_L) + '-r_L' + str(r_L) + 'phi_E{:.2f}'.format(phi_E)
+def job_name(gamma_L, L_00, phi_E):
+    r_L = L_00 * d_r_L[phi_E] # or / gamma_L = (2/3)
+    return 'vary_gamma_L-{:.4f}-L_0-{:.2f}-phi_E{:.2f}'.format(gamma_L, r_L, phi_E)
+
 def prototype():
-    return d_runs[(6e4, 1e6, 0.)]
+    return d_runs[(2/3, 1e0, 0.)]
 default_suffix = '.run.pkl'
 def run():
     runner.setup(L_job_name)
@@ -48,8 +53,9 @@ def make_vary_gamma_L(gamma_L=None, r_L=None, phi_E=0., name=None):
         d_PDE_props=d_PDE_props, set_name='vary_gamma_L')
     return run
 
-def setup(gamma_L, r_L, phi_E):
-    jn = job_name(gamma_L, r_L, phi_E)
+def setup(gamma_L, L_00, phi_E):
+    r_L = L_00 * d_r_L[phi_E] # or / gamma_L = (2/3)
+    jn = job_name(gamma_L, L_00, phi_E)
     run = make_vary_gamma_L(gamma_L=gamma_L, r_L=r_L, phi_E=phi_E, name=jn)
     run.job_name = jn
     with open(jn + '.pkl', 'wb') as hout:
